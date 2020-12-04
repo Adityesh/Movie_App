3f1d6fc2ed81fea83afeb87d3bf862d68f9369bb
Adityesh <adityeshmishra98@gmail.com>
2020-12-04 13:53:42 +0530

    Cleanup and added nav component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {Suspense} from 'react';
 
-function App() {
+const Nav = React.lazy(() => import('./components/Nav/Nav'))
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense fallback={<div>Loading component</div>}>
+    
+      <Nav/>
+    
+    </Suspense>
   );
 }
 
---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className="nav-bar">
+            <div className="nav-logo">
+                App Logo
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
